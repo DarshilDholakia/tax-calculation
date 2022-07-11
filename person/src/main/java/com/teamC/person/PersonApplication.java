ebf15019a4b117377660a07c975597fc3978deeb
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-07-11 16:42:48 +0100

    application.yml

--- person/src/main/java/com/teamC/person/PersonApplication.java
+++ person/src/main/java/com/teamC/person/PersonApplication.java
@@ -2,8 +2,9 @@ package com.teamC.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PersonApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonApplication.class, args);
