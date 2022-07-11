92a1f7cde75b24268e4b249da36e9c7b9b7f6245
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-07-11 12:55:25 +0100

    initialised project

---
+++ src/test/java/com/teamC/taxcalculation/TaxCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamC.taxcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
