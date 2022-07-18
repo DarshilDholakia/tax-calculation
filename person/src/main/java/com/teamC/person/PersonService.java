3bfd442b51e5b6dbd8bcebae3fbf67edcc028251
suraajleal <suraaj_leal@hotmail.co.uk>
2022-07-18 14:14:23 +0100

    finish checks for person

--- person/src/main/java/com/teamC/person/PersonService.java
+++ person/src/main/java/com/teamC/person/PersonService.java
@@ -1,6 +1,7 @@
 package com.teamC.person;
 
 import com.teamC.clients.person.Person;
+import com.teamC.taxcalculation.exception.InvalidRequestException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,19 +19,24 @@ public class PersonService {
     }
 
     public Optional<Person> getPersonById(String id){
+        validateId(id);
         return personRepository.findById(id);
     }
 
     public Person addPerson(Person person){
+        validatePersonInputProperties(person);
         return personRepository.insert(person);
     }
 
     public void deletePersonById(String id){
+        validateId(id);
         personRepository.deleteById(id);
     }
 
     public Person updatePersonById(String id, Person person){
+        validateId(id);
         Optional<Person> existingPerson = getPersonById(id);
+        validatePersonInputProperties(person);
         if (existingPerson.isPresent()) {
             Person updatePerson = existingPerson.get(); //updatePerson will have existingPerson's id
             updatePerson.setFirstName(person.getFirstName());
@@ -45,5 +51,33 @@ public class PersonService {
         }
     }
 
+    public void validateId(String id){
+        if (!(id.matches("[a-zA-Z0-9]*") && id.length()==24)){
+            throw new InvalidRequestException("Id is not valid");
+        };
+    }
 
+    public void validatePersonInputProperties(Person person){
+        if (person.getFirstName().equals("")){
+            throw new InvalidRequestException("First name cannot be blank");
+        } else if (person.getLastName().equals("")){
+            throw new InvalidRequestException("Last name cannot be blank");
+        } else if (person.getEmail().equals("")) {
+            throw new InvalidRequestException("Email name cannot be blank");
+        } else if (person.getEmail().matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")){
+            throw new InvalidRequestException("Email is invalid");
+        } else if (String.valueOf(person.getAge()).equals("")){
+            throw new InvalidRequestException("Age cannot be blank");
+        } else if ((person.getAge())<0){
+            throw new InvalidRequestException("Age cannot be negative");
+        } else if (!(String.valueOf(person.getAge()).matches("[0-9]+"))) {
+            throw new InvalidRequestException("Age must be an integer");
+        } else if (String.valueOf(person.getTaxNumber()).equals("")){
+            throw new InvalidRequestException("Tax number cannot be blank");
+        } else if ((person.getTaxNumber())<0) {
+            throw new InvalidRequestException("Tax number cannot be negative");
+        } else if (!(String.valueOf(person.getTaxNumber()).matches("[0-9]+"))){
+            throw new InvalidRequestException("Tax number must be an integer");
+        }
+    }
 }
--- person/src/test/java/com/teamC/person/PersonServiceTest.java
+++ person/src/test/java/com/teamC/person/PersonServiceTest.java
@@ -1,22 +1,57 @@
 package com.teamC.person;
 
+import com.teamC.clients.person.Person;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonServiceTest {
 
+    private PersonRepository personRepository;
+    private PersonService underTest;
     @BeforeEach
     void setUp() {
+        personRepository = Mockito.mock(PersonRepository.class);
+        underTest = new PersonService(personRepository);
     }
 
     @Test
     void getAllPeople() {
+        //given
+        Person person1 = new Person("1", "John", "Lewis", "johnL@gmail,com", 21, 6565865L);
+        Person person2 = new Person("2", "John2", "Lewis2", "john2L@gmail,com", 22, 7565865L);
+        List<Person> expectedAllPeopleList = Arrays.asList(person1, person2);
+
+        given(personRepository.findAll()).willReturn(expectedAllPeopleList);
+
+        //when
+        List<Person> actualAllPeopleList = underTest.getAllPeople();
+
+        //then
+        assertThat(expectedAllPeopleList).isEqualTo(actualAllPeopleList);
     }
 
     @Test
     void getPersonById() {
+
     }
 
     @Test
