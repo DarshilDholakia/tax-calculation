3153bdb0d4db57ad153963e27dd6fb85e86dae4e
zhengyang qiu <15224003720@163.com>
2022-07-21 14:06:56 +0100

    add some test

--- income/src/test/java/com/teamC/income/IncomeServiceTest.java
+++ income/src/test/java/com/teamC/income/IncomeServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,15 +51,55 @@ class IncomeServiceTest {
 
     @Test
     void getIncomeById() {
+        //given
+        Income income1 = new Income("1","2",20000,30000,500);
+
+        Optional<Income> expectedIncomeById= Optional.of(income1);
+        given(incomeRepository.findById(income1.getId())).willReturn(expectedIncomeById);
+        //when
+        Optional<Income> actualIncomeById=underTest.getIncomeById("1");
+
+        //then
+        assertThat(expectedIncomeById).isEqualTo(actualIncomeById);
+
+
+
 
     }
 
     @Test
     void getIncomeByPersonId() {
+        Person person1= new Person("2", "John", "Lewis", "johnL@gmail,com", 21, 6565865L);
+
+        Income income1 = new Income("1","2",20000,30000,500);
+
+        Income expectedIncomeByPersonId= income1;
+        given(personClient.getPersonById("2","2")).willReturn(Optional.of(person1));
+        given(incomeRepository.findByPersonId(income1.getPersonId())).willReturn(expectedIncomeByPersonId);
+        //when
+        Income actualIncomeByPersonId=underTest.getIncomeByPersonId("2","2");
+
+        //then
+        assertThat(expectedIncomeByPersonId).isEqualTo(actualIncomeByPersonId);
+
+
     }
 
     @Test
     void addIncome() {
+
+        Person person1= new Person("2", "John", "Lewis", "johnL@gmail,com", 21, 6565865L);
+        Income income1 = new Income("1","2",20000,30000,500);
+        Income income2 = new Income("2","3",20000,30000,500);
+        List<Income> expectedAllIncomeList= Arrays.asList(income1,income2);
+        List<Income>IncomeList=Arrays.asList(income1);
+        given(incomeRepository.insert(income1)).willReturn((Income) expectedAllIncomeList);
+        //when
+        List<Income> actualAllIncomeList= (List<Income>) underTest.addIncome("1","1",income2);
+
+        //then
+        assertThat(expectedAllIncomeList).isEqualTo(actualAllIncomeList);
+
     }
 
     @Test
