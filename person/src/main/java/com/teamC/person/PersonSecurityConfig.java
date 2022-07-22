5c5d384b612049d699fbe942dee0c9987a32c9c8
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-07-22 11:02:43 +0100

    API Gateway security working

--- person/src/main/java/com/teamC/person/PersonSecurityConfig.java
+++ person/src/main/java/com/teamC/person/PersonSecurityConfig.java
@@ -1,15 +1,15 @@
-//package com.teamC.person;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//
+package com.teamC.person;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 //@Configuration
 //@EnableWebSecurity
 //public class PersonSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -24,12 +24,4 @@
 //    http.csrf().disable()
 //            .authorizeRequests().antMatchers("/person/**").fullyAuthenticated().and
 //    ().httpBasic().and().csrf().disable(); }
-//
-//
-//    // BCrypt is a hashing algorithm which will be used to encrypt our password
-////    @Bean
-////    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-////        return new BCryptPasswordEncoder();
-////    }
-//
 //}
