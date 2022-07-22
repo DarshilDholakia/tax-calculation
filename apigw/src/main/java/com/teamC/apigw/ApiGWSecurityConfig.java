55bacc072610833f5204dd490cd384a50f3c56d9
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-07-22 10:47:44 +0100

    api gateway security configured

--- apigw/src/main/java/com/teamC/apigw/ApiGWSecurityConfig.java
+++ apigw/src/main/java/com/teamC/apigw/ApiGWSecurityConfig.java
@@ -1,21 +1,41 @@
 package com.teamC.apigw;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class ApiGWSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("User").password("{noop}Password").roles("USER");
+@EnableWebFluxSecurity
+public class ApiGWSecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("User")
+                .password("Password")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
     }
 
-    @Override protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
         http
-                .authorizeRequests().antMatchers("/person/**", "incomes/**", "/tax-calculations/**").fullyAuthenticated().and
-                        ().httpBasic().and().csrf().disable(); }
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/person/**").permitAll()
+                .pathMatchers("/incomes/**").permitAll()
+                .pathMatchers("/tax-calculations/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
 }
