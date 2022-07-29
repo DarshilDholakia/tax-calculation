ef6497870597a70f7fc22fe87095781406963179
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-07-29 10:01:38 +0100

    docker feature with bugs

--- config-server/src/main/java/com/teamC/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/teamC/config/ConfigServerApplication.java
@@ -1,13 +1,17 @@
 package com.teamC.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+//        ApplicationContext ctx = new SpringApplicationBuilder(ConfigServerApplication.class).web(WebApplicationType.NONE).run(args);
     }
 }
