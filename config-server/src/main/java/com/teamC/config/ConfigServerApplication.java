2fc65826e31517927d0bdaf305e7bcd14eadf84f
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-08-01 14:48:09 +0100

    exclusion of tomcat transitive dependency added

--- config-server/src/main/java/com/teamC/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/teamC/config/ConfigServerApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
 //        ApplicationContext ctx = new SpringApplicationBuilder(ConfigServerApplication.class).web(WebApplicationType.NONE).run(args);
